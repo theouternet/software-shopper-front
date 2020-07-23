2d7fb178995d320a06435876251598199526d59b
theouternet <jrougeau@gmail.com>
2020-07-23 12:12:33 -0500

    TT Reducers

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,34 +1,10 @@
-import { combineReducers } from "redux";
-
-const primaryReducer = (state = [], action) => {
-    switch(action.type) {
-     
-
-
-        case "SEARCH_RESULTS":
-            return action.payload
-
-
-
-        default: 
-            return state;
+export default (state = {loading: false, pictures: []}, action) => {
+    switch (action.type) {
+      case 'FETCH_SOFTWARE':
+        return {loading: false, pictures: action.payload};
+      case 'LOADING_SOFTWARE':
+        return {loading: true, pictures: action.payload};
+      default:
+        return state;
     }
-}
-
-
-const searchReducer = (state = "", action) => {
-    switch(action.type){
-        case "MODIFY_SEARCH":
-            return action.payload
-        default: 
-            return state;
-    }
-}
-
-
-const rootReducer = combineReducers({
-    softwareState: primaryReducer,
-    searchText: searchReducer
-});
-
-export default rootReducer;
+  };
